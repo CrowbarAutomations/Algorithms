14362e8bcd018ac92a4316cca137e229445cab90
Corbin McCallum <137533488+CrowbarAutomations@users.noreply.github.com>
2023-06-23 16:40:23 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_COMMAND_LENGTH 1
+#define MAX_COMMAND_LENGTH 1 // Maximum length of the command (algorithm name)
 
 int main() {
-    char command[MAX_COMMAND_LENGTH];
+    char command[MAX_COMMAND_LENGTH]; // Array to store the command (algorithm name)
   
+    // Print the list of available algorithms
     printf("The following algorithms can be selected:\n",
               "Quick Sort\n",
               "Dijkstra's Algorithm\n",
@@ -15,9 +16,14 @@ int main() {
       );
   
     printf("Enter the name of the algorithm which you want to run:");
+    
+    // Read the user's input (algorithm name) and store it in the 'command' variable
     fgets(command, sizeof(command), stdin);
+    
+    // Remove the newline character at the end of the input
     command[strcspn(command, "\n")] = '\0'; 
 
+    // Print the selected algorithm
     printf("The following algorithm has been selected: %s\n", command);
   
     return 0;
