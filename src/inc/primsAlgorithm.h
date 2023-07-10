eadedc7a6d4bb31cb4e56bfb8b22a70dd83665d1
CrowbarAutomations <crowbarautomations@gmail.com>
2023-07-10 13:25:33 -0400

    Implemented the code for primsAlgorithm and updated the header file

--- src/inc/primsAlgorithm.h
+++ src/inc/primsAlgorithm.h
@@ -3,11 +3,23 @@
 
 // Libraries
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 
 // Local Files
 #include "fileProcessing.h"
 
+#define MAX_VERTICES 100
+
+// Function prototype for Prim's Algorithm
 void primsAlgorithm(FileData *data);
+
+// Utility function to find the vertex with the minimum key value
+int minKey(int key[], bool mstSet[], int numVertices);
+
+// Utility function to print the selected edges of the minimum spanning tree
+void printMST(int graph[][MAX_VERTICES], int numVertices);
+
+// Function to find the minimum spanning tree using Prim's Algorithm
+int primMST(int graph[][MAX_VERTICES], int numVertices);
 
 #endif /* PRIMS_ALGORITHM_H */
--- src/primsAlgorithm.c
+++ src/primsAlgorithm.c
@@ -1,15 +1,120 @@
 #include "primsAlgorithm.h"
 
+#define INF 9999999
+
 void primsAlgorithm(FileData *data)
 {
   // Access the data from the FileData structure
-  int value1 = data->value1;
-  float value2 = data->value2;
   int numValues = data->numValues;
   float *values = data->values;
 
-  // Algorithm implementation using the data
-  // ...
+  // Check if there are enough values for the algorithm
+  if (numValues < 1)
+  {
+    printf("Insufficient values for Prim's Algorithm\n");
+    return;
+  }
 
-  printf("Executing Prim's Algorithm\n");
+  // Calculate the number of vertices based on the number of values
+  int numVertices = (int)values[0];
+
+  // Calculate the number of edges
+  int numEdges = numValues - 1;
+
+  // Check if the number of edges is correct
+  if (numEdges != numVertices * (numVertices - 1) / 2)
+  {
+    printf("Invalid number of edges\n");
+    return;
+  }
+
+  // Create an adjacency matrix to represent the graph
+  int graph[numVertices][numVertices];
+
+  // Fill the adjacency matrix with values from the data array
+  int dataIndex = 1;
+  for (int i = 0; i < numVertices; i++)
+  {
+    for (int j = 0; j < numVertices; j++)
+    {
+      graph[i][j] = (int)values[dataIndex++];
+    }
+  }
+
+  // Perform Prim's Algorithm to find the minimum spanning tree
+  int minCost = primMST(graph, numVertices);
+
+  // Print the minimum cost and the selected edges
+  printf("Minimum Cost: %d\n", minCost);
+  printf("Selected Edges:\n");
+  printMST(graph, numVertices);
+}
+
+int minKey(int key[], bool mstSet[], int numVertices)
+{
+  int min = INF, minIndex;
+
+  // Find the vertex with the minimum key value among the vertices not yet included in the MST
+  for (int v = 0; v < numVertices; v++)
+  {
+    if (mstSet[v] == false && key[v] < min)
+    {
+      min = key[v];
+      minIndex = v;
+    }
+  }
+
+  return minIndex;
+}
+
+void printMST(int graph[][MAX_VERTICES], int numVertices)
+{
+  // Print the selected edges of the minimum spanning tree
+  for (int i = 1; i < numVertices; i++)
+  {
+    printf("%d - %d\n", i, graph[i][parent[i]]);
+  }
+}
+
+int primMST(int graph[][MAX_VERTICES], int numVertices)
+{
+  int parent[numVertices];  // Array to store the parent of each vertex in the MST
+  int key[numVertices];     // Array to store the key values of each vertex
+  bool mstSet[numVertices]; // Array to track which vertices are included in the MST
+
+  // Initialize the key values and MST set arrays
+  for (int i = 0; i < numVertices; i++)
+  {
+    key[i] = INF;
+    mstSet[i] = false;
+  }
+
+  key[0] = 0; // Start with the first vertex as the root of the MST
+  parent[0] = -1;
+
+  // Construct the MST with (numVertices - 1) edges
+  for (int count = 0; count < numVertices - 1; count++)
+  {
+    int u = minKey(key, mstSet, numVertices); // Find the vertex with the minimum key value
+    mstSet[u] = true;                         // Mark the vertex as included in the MST
+
+    // Update the key values and parent for adjacent vertices
+    for (int v = 0; v < numVertices; v++)
+    {
+      if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
+      {
+        parent[v] = u;
+        key[v] = graph[u][v];
+      }
+    }
+  }
+
+  // Calculate the minimum cost by summing up the edge weights in the MST
+  int minCost = 0;
+  for (int i = 1; i < numVertices; i++)
+  {
+    minCost += graph[i][parent[i]];
+  }
+
+  return minCost;
 }
